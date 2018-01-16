fe633a257c576c2c7f54a82ca48728f81353aa93
joope <pyry.joona@helsinki.fi>
2018-01-16 18:23:29 +0200

    1.7

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -13,20 +13,26 @@ class App extends Component {
     }
 
     render () {
-        const { hyva, neutraali, huono } = this.state;
+        const { hyva, neutraali, huono } = this.state
+        const yhteensa = hyva + neutraali + huono
+        const keskiarvo = (hyva - huono) / yhteensa
+        const positiivisia = hyva / yhteensa
+
 
         return (
             <div>
                 <h1>anna palautetta</h1>
                 <button onClick={() => this.setState({hyva: hyva + 1})}>hyvä</button>
                 <button onClick={() => this.setState({neutraali: neutraali + 1})} >neutraali</button>
-                <button onClick={() => this.setState({huono: hyva + 1})}>huono</button>
+                <button onClick={() => this.setState({huono: huono + 1})}>huono</button>
 
                 <h1>statistiikka</h1>
                 <p>
                     hyvä: {hyva} <br/>
                     neutraali: {neutraali} <br/>
-                    huono: {huono}
+                    huono: {huono} <br/>
+                    keskiarvo: {keskiarvo.toFixed(2)} <br/>
+                    positiivisia: {(positiivisia * 100).toFixed(1)}% <br/>
                 </p>
 
             </div>
